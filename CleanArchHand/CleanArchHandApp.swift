b9a73bb83e017d7789f9fd877b77e8085db2ddfd
Laura Franco Peñaranda <laura@digio.es>
2022-03-06 17:37:46 +0100

    Kick off

---
+++ CleanArchHand/CleanArchHandApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchHandApp.swift
+//  CleanArchHand
+//
+//  Created by Laura on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchHandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
